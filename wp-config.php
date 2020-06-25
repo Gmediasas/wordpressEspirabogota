2713d81fb0a54798c3bae3fc18810dd574797bea
SatovaA <2919bpqpaola@gmail.com>
2020-06-25 15:32:56 -0500

    cambio uno

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'aspirabogota');
+define('DB_NAME', 'espirabogota');
 
 /** MySQL database username */
 define('DB_USER', 'root');
@@ -29,8 +29,8 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhot');
 
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
